64820eb6cb3782e80ec2a8e1a31891e53884a941
Tee Ming <chewteeming01@gmail.com>
2024-01-14 15:59:44 +0800

    add example with svg

--- src/routes/og/Card.svelte
+++ src/routes/og/Card.svelte
@@ -1,10 +1,15 @@
 <!-- Thanks to https://geoffrich.net/posts/svelte-social-image/ -->
 <script lang="ts">
+	import logo from './logo.svg?raw';
+
 	export let title: string;
 </script>
 
 <div class="card">
 	<h1>{title}</h1>
+	<div class="logo">
+		{@html logo}
+	</div>
 </div>
 
 <style>
@@ -19,5 +24,26 @@
 		align-items: center;
 		justify-content: center;
 		text-align: center;
+	}
+
+	.logo {
+		background-color: #fff;
+		padding: 1rem;
+		border-radius: 100%;
+
+		/* CSS variables are not allowed */
+		/* --size: 100px;
+		width: var(--size);
+		height: var(--size); */
+		width: 150px;
+		height: 150px;
+
+		/** grid is not allowed */
+		/* display: grid;
+		place-content: center; */
+
+		display: flex;
+		justify-content: center;
+		align-items: center;
 	}
 </style>
