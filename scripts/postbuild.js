c2e664d23e1f94a0e7b755e07c96e080e88fb47d
Tee Ming <chewteeming01@gmail.com>
2024-01-18 23:40:00 +0800

    try again

--- scripts/postbuild.js
+++ scripts/postbuild.js
@@ -8,3 +8,7 @@ fs.copyFileSync(
 	'node_modules/@vercel/og/dist/noto-sans-v27-latin-regular.ttf',
 	'.vercel/output/functions/fn-0.func/noto-sans-v27-latin-regular.ttf'
 );
+fs.copyFileSync(
+	'node_modules/@vercel/og/dist/noto-sans-v27-latin-regular.ttf',
+	'.vercel/output/functions/fn.func/noto-sans-v27-latin-regular.ttf'
+);
