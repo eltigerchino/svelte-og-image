a52341a9ef2e2e7c96eae50ef7abce3b21ed6d93
Tee Ming <chewteeming01@gmail.com>
2024-01-18 23:36:58 +0800

    missed a function

--- scripts/postbuild.js
+++ scripts/postbuild.js
@@ -1,3 +1,10 @@
 import fs from 'node:fs';
 
-fs.copyFileSync('node_modules/@vercel/og/dist/noto-sans-v27-latin-regular.ttf', '.vercel/output/functions/fn-1.func/noto-sans-v27-latin-regular.ttf')
+fs.copyFileSync(
+	'node_modules/@vercel/og/dist/noto-sans-v27-latin-regular.ttf',
+	'.vercel/output/functions/fn-1.func/noto-sans-v27-latin-regular.ttf'
+);
+fs.copyFileSync(
+	'node_modules/@vercel/og/dist/noto-sans-v27-latin-regular.ttf',
+	'.vercel/output/functions/fn-0.func/noto-sans-v27-latin-regular.ttf'
+);
