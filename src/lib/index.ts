f9d9dd48059903afa7aaeca87527b20d20c26fc6
Tee Ming <chewteeming01@gmail.com>
2025-07-31 15:35:10 +0800

    feat: add warning when css is not injected

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -3,7 +3,8 @@ import { html } from 'satori-html';
 import { ImageResponse as VercelOGImageResponse } from '@vercel/og';
 import type { ImageResponseOptions } from './types.js';
 import { render } from 'svelte/server';
- 
+import { DEV } from 'esm-env';
+
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export class ImageResponse<TComponent extends Component<any>> extends VercelOGImageResponse {
 	constructor(
@@ -11,6 +12,11 @@ export class ImageResponse<TComponent extends Component<any>> extends VercelOGIm
 		props: ComponentProps<TComponent>,
 		options: ImageResponseOptions
 	) {
+		if (DEV && !component.toString().includes('$$payload.css.add($$css);')) {
+			throw new Error(
+				'Missing `<svelte:options css="injected" />` in the component. The option must be set to inject CSS into the image response.'
+			);
+		}
 		// @ts-expect-error what the heck is the problem??
 		const result = render(component, { props });
 		const element = html(`${result.body}${result.head}`);
